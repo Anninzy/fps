ee057887a5e244bc434ddeede15e1f333ba0fbe5
Anninzy <143312793+Anninzy@users.noreply.github.com>
2023-11-21 14:48:44 +0800

    Untested react UI

--- src/ReplicatedFirst/Client/Scripts/GuiController.lua
+++ src/ReplicatedFirst/Client/Scripts/GuiController.lua
@@ -1,12 +1,30 @@
 local StarterGui = game:GetService("StarterGui")
 local UserInputService = game:GetService("UserInputService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
 local react = _G.react
 
 StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.All, false)
 StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, true)
-StarterGui:SetCsrc/ReplicatedFirst/Client/Scripts/GuiController.luaoreGuiEnabled(Enum.CoreGuiType.PlayerList, true)
 
 UserInputService.MouseIconEnabled = false
+
+local app = react.createElement("ScreenGui", {
+    IgnoreGuiInset = false
+}, {
+    Magazine = react.createElement("TextLabel", {
+      Size = UDim2.new(0.1, 0, 0.1, 0),
+      Text = "25"
+    }, {
+        Reserve = react.createElement("TextLabel", {
+            Anchor = UDim.new(1, 0),
+            Position = UDim2.new(1, 0, 0, 0),
+            Size = UDim2.new(0.1, 0, 0.1, 0),
+            Text = "50"
+        })
+    })
+})
+
+react.mount(app, Players.LocalPlayer.PlayerGyu)
 
 return {}
