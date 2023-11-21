7fbbd7b81dd6c1161c8e25792c7b5a5ed2f92841
Anninzy <143312793+Anninzy@users.noreply.github.com>
2023-11-21 09:16:35 +0800

    Untested recoil change

--- src/ReplicatedFirst/Client/Scripts/RecoilController.lua
+++ src/ReplicatedFirst/Client/Scripts/RecoilController.lua
@@ -11,15 +11,26 @@ function module.StartRecoil()
 		preRender:Disconnect()
 	end
 
+	local framesRan = 0
 	local alpha = 0
 	local increment = 0.05
 	totalOffsetAppliedUp = CFrame.Angles(0, 0, 0)
 
 	preRender = RunService.PreRender:Connect(function()
-		alpha = math.clamp(alpha + increment, 0, 1)
+		local actualIncrement
+		local typeOfMovement = framesRan % 3 --0: Upward cam movement, 1: No movement, 2: Halved downwards cam movement
+		if typeOfMovement == 1 then
+			return
+		elseif typeOfMovement == 2 then
+			actualIncrement = -increment / 2
+		end
+
+		alpha = math.clamp(alpha + actualIncrement, 0, 1)
 		local cameraCFrameNoOffset = camera.CFrame * totalOffsetAppliedUp:Inverse()
 		totalOffsetAppliedUp = CFrame.Angles(0, 0, 0):Lerp(cameraAngleOffset, alpha)
 		camera.CFrame = cameraCFrameNoOffset * totalOffsetAppliedUp
+
+		framesRan += 1
 	end)
 end
 
