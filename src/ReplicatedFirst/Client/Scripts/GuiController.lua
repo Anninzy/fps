eb9cabffc0bcf7ec035aea606aeeea3da84fbf36
Anninzy <143312793+Anninzy@users.noreply.github.com>
2023-11-21 15:10:00 +0800

    Untested react HUD

--- src/ReplicatedFirst/Client/Scripts/GuiController.lua
+++ src/ReplicatedFirst/Client/Scripts/GuiController.lua
@@ -9,22 +9,47 @@ StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, true)
 
 UserInputService.MouseIconEnabled = false
 
-local app = react.createElement("ScreenGui", {
-    IgnoreGuiInset = false
-}, {
-    Magazine = react.createElement("TextLabel", {
-      Size = UDim2.new(0.1, 0, 0.1, 0),
-      Text = "25"
+local hud = react.Component:extend("HUD")
+
+function hud:Init()
+    self:setState({
+        magazine = "25",
+        reserve = "50"
+    })
+end
+
+function hud:render()
+    local state = self.state
+
+    return react.createElement("ScreenGui", {
+        IgnoreGuiInset = false
     }, {
-        Reserve = react.createElement("TextLabel", {
-            Anchor = UDim.new(1, 0),
-            Position = UDim2.new(1, 0, 0, 0),
-            Size = UDim2.new(0.1, 0, 0.1, 0),
-            Text = "50"
+        Magazine = react.createElement("TextLabel", {
+          Size = UDim2.new(0.1, 0, 0.1, 0),
+          Text = state.magazine
+        }, {
+            Reserve = react.createElement("TextLabel", {
+                Anchor = UDim.new(1, 0),
+                Position = UDim2.new(1, 0, 0, 0),
+                Size = UDim2.new(0.1, 0, 0.1, 0),
+                Text = state.reserve
+            })
         })
     })
-})
+end
 
-react.mount(app, Players.LocalPlayer.PlayerGyu)
+function hud:didMount()
+    --on ammo change, update
+    --[[
+    self:setState(function(state)
+        return {
+            magazine = "",
+            reserve = ""
+        }
+    end)
+    ]]
+end
+
+react.mount(react.createElement(hud), Players.LocalPlayer.PlayerGui, "HUD")
 
 return {}
