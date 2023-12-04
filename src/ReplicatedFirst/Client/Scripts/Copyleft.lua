5ba57013c8432f32ab08c39c4afdd6b38adff168
Anninzy <proxied@skiff.com>
2023-12-04 16:50:03 +0800

    Added copyleft

---
+++ src/ReplicatedFirst/Client/Scripts/Copyleft.lua
@@ -0,0 +1,19 @@
+print([[                                                       
+uuuuuu    uuuuuuwwwwwww           wwwww           wwwwwwwuuuuuu    uuuuuu  
+u::::u    u::::u w:::::w         w:::::w         w:::::w u::::u    u::::u  
+u::::u    u::::u  w:::::w       w:::::::w       w:::::w  u::::u    u::::u  
+u::::u    u::::u   w:::::w     w:::::::::w     w:::::w   u::::u    u::::u  
+u::::u    u::::u    w:::::w   w:::::w:::::w   w:::::w    u::::u    u::::u  
+u::::u    u::::u     w:::::w w:::::w w:::::w w:::::w     u::::u    u::::u  
+u::::u    u::::u      w:::::w:::::w   w:::::w:::::w      u::::u    u::::u  
+u:::::uuuu:::::u       w:::::::::w     w:::::::::w       u:::::uuuu:::::u  
+u:::::::::::::::uu      w:::::::w       w:::::::w        u:::::::::::::::uu
+ u:::::::::::::::u       w:::::w         w:::::w          u:::::::::::::::u
+  uu::::::::uu:::u        w:::w           w:::w            uu::::::::uu:::u
+    uuuuuuuu  uuuu         www             www               uuuuuuuu  uuuu
+                                                 
+Licensed under AGPL-3.0
+https://github.com/Anninzy
+]])
+
+return {}
