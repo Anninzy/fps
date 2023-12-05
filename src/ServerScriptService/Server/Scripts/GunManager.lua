95b503cee8f8ac882affbd822c0f29c380334263
Anninzy <143312793+Anninzy@users.noreply.github.com>
2023-12-05 11:21:08 +0800

    WIP Wallbang

--- src/ServerScriptService/Server/Scripts/GunManager.lua
+++ src/ServerScriptService/Server/Scripts/GunManager.lua
@@ -19,9 +19,10 @@ local function raycastBullet(player: Player, mouseUnitRayDirection: Vector3, spr
 		end
 	end
 
+	local character = player.Character
 	gunFiredTime[player] = os.clock()
 
-	return _G.RaycastBullet(player, mouseUnitRayDirection, spread)
+	return _G.RaycastBullet(character, character.Head.Position, mouseUnitRayDirection, spread)
 end
 
 local function createBulletHole(playerWhoFired: Player, raycastResult: RaycastResult)
@@ -34,33 +35,9 @@ local function createBulletHole(playerWhoFired: Player, raycastResult: RaycastRe
 	end
 end
 
-Players.PlayerAdded:Connect(function(player: Player)
-	playersCurrentGun[player] = "Vandal"
-	remotesFolder.ChangeGun:FireClient(player, "Vandal")
-
-	player.CharacterAdded:Connect(function()
-		healthChangedEvent:FireClient(player, 100)
-		shieldChangedEvent:FireClient(player, 0)
-	end)
-end)
-
-Players.PlayerRemoving:Connect(function(player: Player)
-	playersCurrentGun[player] = nil
-end)
-
-remotesFolder.BulletHitSurface.OnServerEvent:Connect(
-	function(playerWhoFired: Player, mouseUnitRayDirection: Vector3, spread: Vector3)
-		local raycastResult = raycastBullet(playerWhoFired, mouseUnitRayDirection, spread)
-
-		if raycastResult then
-			createBulletHole(playerWhoFired, raycastResult)
-		end
-	end
-)
-
-remotesFolder.BulletHitCharacter.OnServerEvent:Connect(
-	function(playerWhoFired: Player, mouseUnitRayDirection: Vector3, spread: Vector3)
-		local raycastResult = raycastBullet(playerWhoFired, mouseUnitRayDirection, spread)
+local function raycastDamage(playerWhoFired: Player, mouseUnitRayDirection: Vector3, spread: Vector3)
+	local character = playerWhoFired.Character
+	local raycastResult = raycastBullet(playerWhoFired, mouseUnitRayDirection, spread)
 
 		if raycastResult then
 			local instance = raycastResult.Instance
@@ -93,6 +70,50 @@ remotesFolder.BulletHitCharacter.OnServerEvent:Connect(
 				createBulletHole(playerWhoFired, raycastResult)
 			end
 		end
+end
+
+Players.PlayerAdded:Connect(function(player: Player)
+	playersCurrentGun[player] = "Vandal"
+	remotesFolder.ChangeGun:FireClient(player, "Vandal")
+
+	player.CharacterAdded:Connect(function()
+		healthChangedEvent:FireClient(player, 100)
+		shieldChangedEvent:FireClient(player, 0)
+	end)
+end)
+
+Players.PlayerRemoving:Connect(function(player: Player)
+	playersCurrentGun[player] = nil
+end)
+
+remotesFolder.BulletHitSurface.OnServerEvent:Connect(
+	function(playerWhoFired: Player, mouseUnitRayDirection: Vector3, spread: Vector3)
+		local raycastResult = raycastBullet(playerWhoFired, mouseUnitRayDirection, spread)
+
+		if raycastResult then
+			local instance = raycastResult.Instance
+			local position = raycastResult.Position
+			local instancePenetration = instance:GetAttribute("Penetration")
+
+			createBulletHole(playerWhoFired, raycastResult, false)
+
+			if not instance:GetAttribute("Penetration") then
+				return
+			end
+
+			if playersCurrentGun[playerWhoFired]["Penetration"] < instancePenetration then
+				return
+			end
+
+			local wallbangRaycastResult = _G.RaycastBullet(playerWhoFired.Character, position + mouseUnitRayDirection * 10, -mouseUnitRayDirection, Vector3.new(0, 0, 0))
+			--wall bang
+		end
+	end
+)
+
+remotesFolder.BulletHitCharacter.OnServerEvent:Connect(
+	function(playerWhoFired: Player, mouseUnitRayDirection: Vector3, spread: Vector3)
+		raycastDamage(playerWhoFired, mouseUnitRayDirection, spread)
 	end
 )
 
